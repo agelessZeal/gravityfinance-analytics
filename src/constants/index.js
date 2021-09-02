2b1190263fa28eaa1a053a5b067f281377a74526
agelessZeal <35507996+agelessZeal@users.noreply.github.com>
2021-09-02 12:14:42 +0300

    change the factory address for subgraph api

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0x3ed75aff4094d2aaa38fafca64ef1c152ec1cf20'
 
 export const BUNDLE_ID = '1'
 
@@ -13,8 +13,8 @@ export const timeframeOptions = {
 
 // token list urls to fetch tokens from - use for warnings on tokens and pairs
 export const SUPPORTED_LIST_URLS__NO_ENS = [
-  'https://gateway.ipfs.io/ipns/tokens.uniswap.org',
-  'https://www.coingecko.com/tokens_list/uniswap/defi_100/v_0_0_0.json',
+  // 'https://gateway.ipfs.io/ipns/tokens.uniswap.org',
+  // 'https://www.coingecko.com/tokens_list/uniswap/defi_100/v_0_0_0.json',
 ]
 
 // hide from overview list
@@ -36,10 +36,7 @@ export const TOKEN_BLACKLIST = [
 
 // pair blacklist
 export const PAIR_BLACKLIST = [
-  '0xb6a741f37d6e455ebcc9f17e2c16d0586c3f57a5',
-  '0x97cb8cbe91227ba87fc21aaf52c4212d245da3f8',
-  '0x1acba73121d5f63d8ea40bdc64edb594bd88ed09',
-  '0x7d7e813082ef6c143277c71786e5be626ec77b20',
+
 ]
 
 // warnings to display if page contains info about blocked token
@@ -51,16 +48,11 @@ export const BLOCKED_WARNINGS = {
 /**
  * For tokens that cause erros on fee calculations
  */
-export const FEE_WARNING_TOKENS = ['0xd46ba6d942050d489dbd938a2c909a5d5039a161']
+export const FEE_WARNING_TOKENS = []
 
 export const UNTRACKED_COPY = 'Derived USD values may be inaccurate without liquid stablecoin or ETH pairings.'
 
 // tokens that should be tracked but arent due to lag in subgraph
 export const TRACKED_OVERRIDES = [
-  '0x9928e4046d7c6513326ccea028cd3e7a91c7590a',
-  '0x87da823b6fc8eb8575a235a824690fda94674c88',
-  '0xcd7989894bc033581532d2cd88da5db0a4b12859',
-  '0xe1573b9d29e2183b1af0e743dc2754979a40d237',
-  '0x45804880de22913dafe09f4980848ece6ecbaf78',
-  '0x709f7b10f22eb62b05913b59b92ddd372d4e2152',
+
 ]
